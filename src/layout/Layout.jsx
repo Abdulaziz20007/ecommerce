cd82eae2775257b7a2fd1058bd4b44cb43ebadf8
Abdulaziz20007 <lil.gangsser@gmail.com>
2025-05-09 13:32:32 +0500

    done

---
+++ src/layout/Layout.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Header from './Header/Header';
+import { Outlet } from 'react-router';
+import Footer from './Footer/Footer';
+
+
+function Layout() {
+  return (
+    <div>
+        <Header/>
+        <main>
+            <Outlet />
+        </main>
+        <Footer />
+    </div>
+  )
+}
+
+export default Layout;
